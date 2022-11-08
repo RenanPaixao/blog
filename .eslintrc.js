e423d76a83d66c2d9533830121f1124911e93f08
renan <renanlemos21@gmail.com>
2022-11-07 23:15:57 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: '2020'
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  rules: {
+    'padded-blocks': ['warn', 'never'],
+    'space-before-function-paren': ['warn', 'never'],
+    'no-mixed-spaces-and-tabs': 'off',
+    quotes: ['error', 'single'],
+    'vue/multi-word-component-names': 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    semi: ['error', 'never'],
+    'no-duplicate-imports': 'error',
+    strict: 'error',
+    'sort-imports': 'error',
+    'import/order': 'off',
+    'no-tabs': 'off'
+  }
+}
