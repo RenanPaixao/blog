62c37cec2fdcbace7aefd4d62a0ff2ec922b3b8d
renan <renanlemos21@gmail.com>
2022-11-15 22:59:39 -0300

    feat: add not found slot

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1 class="text-6xl">
+      Page not found!
+    </h1>
+    <button>Back</button>
+  </main>
+</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,12 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <DocumentDrivenNotFound />
+      </template>
+    </ContentDoc>
   </main>
 </template>
+<script setup lang="ts">
+import { DocumentDrivenNotFound } from '#components'
+</script>
