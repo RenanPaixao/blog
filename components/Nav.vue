7c78eb5af73f408c6c2f606e2a09999fc54de139
renan <renanlemos21@gmail.com>
2022-11-17 23:11:11 -0300

    feat: add body size and initial navbar

---
+++ components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="w-100 py-3 pr-2 bg-indigo-500">
+    <ul class="w-100 flex justify-end items-center">
+      <li v-for="[title, path] in navLinks" :key="path" class="mx-2">
+        <NuxtLink class="text-slate-300" :to="path">
+          {{ title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+const navLinks = [
+  ['Home', '/'],
+  ['Posts', '/posts'],
+  ['Projects', '/projects']
+]
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,25 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
+  alias: {
+    styles: resolve(__dirname, 'assets/styles')
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
   ],
+
   content: {
     documentDriven: true
+  },
+
+  css: ['styles/reset.scss'],
+
+  tailwindcss: {
+    injectPosition: {
+      after: 'styles/reset.scss'
+    }
   }
 })
