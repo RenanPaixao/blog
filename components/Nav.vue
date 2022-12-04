7a835cb0524b3aacdfdd96dcc6acb489acea3a9f
renan <renanlemos21@gmail.com>
2022-12-04 16:25:05 -0300

    feat: add home page logo

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,8 +1,11 @@
 <template>
-  <nav class="w-100 py-3 pr-2 bg-indigo-500">
+  <nav class="flex justify-between w-100 py-2 pr-4 pl-6 bg-indigo-500">
+    <NuxtLink to="/">
+      <img src="../assets/images/logo-w.png" class="hover:backdrop-brightness-75" alt="logo">
+    </NuxtLink>
     <ul class="w-100 flex justify-end items-center">
       <li v-for="[title, path] in navLinks" :key="path" class="mx-2">
-        <NuxtLink class="text-slate-300" :to="path">
+        <NuxtLink class="text-slate-300 hover:text-slate-50" :to="path">
           {{ title }}
         </NuxtLink>
       </li>
@@ -13,7 +16,6 @@
 <script lang="ts" setup>
 
 const navLinks = [
-  ['Home', '/'],
   ['Posts', '/posts'],
   ['Projects', '/projects']
 ]
