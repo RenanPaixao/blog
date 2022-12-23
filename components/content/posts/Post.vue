f2f8667bec5bd81a318264f98e39bf278d04817a
renan <renanlemos21@gmail.com>
2022-12-23 03:22:05 -0300

    feat: add navigation to a post

--- components/content/posts/Post.vue
+++ components/content/posts/Post.vue
@@ -3,13 +3,14 @@
     class="
     border border-2 bg-[#B7A2F6] dark:bg-[#19074B] rounded-lg mb-3 pb-2 hover:cursor-pointer
     hover:scale-105 transform transition duration-200 ease-in-out"
+    @click="navigateToPost"
   >
     <header class="px-3">
       <h2 class="mt-2 mb-3">
-        {{ post.title }}
+        {{ props.post.title }}
       </h2>
       <p class="mb-0">
-        {{ post.description || 'No description' }}
+        {{ props.post.description || 'No description' }}
       </p>
       <small>
         <time>7 min read</time>
@@ -20,6 +21,7 @@
 
 <script lang="ts" setup>
 import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
+import { navigateTo } from '#app'
 
 interface Post extends MarkdownParsedContent{
 	_path: string
@@ -32,8 +34,12 @@ interface Post extends MarkdownParsedContent{
 	_extension: string
 }
 
-defineProps<{
+const props = defineProps<{
 	post: Post
 }>()
+
+function navigateToPost():void {
+  navigateTo(props.post._path)
+}
 
 </script>
