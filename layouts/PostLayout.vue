abf0fdf3ef3e370581016f889af6f4fad9bb17b5
renan <renanlemos21@gmail.com>
2022-12-27 22:33:18 -0300

    feat: add post back button

---
+++ layouts/PostLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <slot />
+    <div class="flex justify-end">
+      <button
+        class="text-text-color hover:text-black dark:text-primary dark:hover:text-soft-dark"
+        @click="router.back()"
+      >
+        <icon name="mdi:arrow-left-thin" /> Back
+      </button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from '#app'
+
+const router = useRouter()
+</script>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,21 @@
 <template>
   <main class="prose prose-gray prose-h1:pl-5 dark:prose-invert mx-auto pt-10">
-    <ContentDoc tag="section">
-      <template #not-found>
-        <DocumentDrivenNotFound />
-      </template>
-    </ContentDoc>
+    <NuxtLayout :name="currentLayout">
+      <ContentDoc tag="section">
+        <template #not-found>
+          <DocumentDrivenNotFound />
+        </template>
+      </ContentDoc>
+    </NuxtLayout>
   </main>
 </template>
 <script setup lang="ts">
+import { computed, useRoute } from '#imports'
 import { DocumentDrivenNotFound } from '#components'
+
+const route = useRoute()
+
+const currentLayout = computed(() => {
+  return route.path.includes('/posts/') ? 'post-layout' : ''
+})
 </script>
