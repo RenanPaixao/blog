cc19c064e6e7e31f43999d4e06dc5d43d534d659
renan <renanlemos21@gmail.com>
2023-01-21 16:25:16 -0300

    fix flickering when get out of the blog and then come back

--- app.vue
+++ app.vue
@@ -6,17 +6,22 @@
 </template>
 
 <script lang="ts" setup>
+import { useAsyncData } from '#app'
 import { useHead } from '#imports'
 
-useHead({
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png'
-    }
-  ]
+// This way of useHead use avoid flickering when get out of the blog and back again.
+useAsyncData(async() => {
+  await useHead({
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png'
+      }
+    ]
+  })
 })
+
 </script>
 <style lang="scss">
 // Avoid flickering on page load
