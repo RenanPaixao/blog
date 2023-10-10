70e4ab55730f6ecd5960367ba6a575aeff0f455b
renan <renanlemos21@gmail.com>
2023-10-10 11:43:29 -0300

    Revert "feat: enable prerender to all routes"
    This reverts commit 59a47aa7a2bce79819dfe74c4dc89c6f69265214.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,15 +5,13 @@ export default defineNuxtConfig({
   alias: {
     '%': resolve(__dirname, 'components/content/')
   },
+
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
     '@nuxtjs/color-mode'
   ],
-  routeRules: {
-    '/**': { isr: true }
-  },
   colorMode: {
     classSuffix: ''
   },
